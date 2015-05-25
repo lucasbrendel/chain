1463ca1b876df2f0ba97f723e32b6baf1ff320ea
Lucas Brendel <lucasbrendel@gmail.com>
2015-05-25 18:21:37 -0500

    Adding the two main classes. The Chain and the Link

---
+++ Chain/Chain.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chain
+{
+    public interface IChain
+    {
+        IList<ILink> Links { get; }
+        void ConnectAllLinks();
+        void LoopLinks();
+        void RunLinks();
+    }
+
+    public class Chain : IChain
+    {
+        private IList<ILink> _Links;
+
+        public Chain()
+        {
+
+        }
+
+        public IList<ILink> Links
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public void ConnectAllLinks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LoopLinks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RunLinks()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Chain/Link.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chain
+{
+    public enum LinkResult
+    {
+        Unknown,
+        Pass,
+        Fail,
+        Inconclusive,
+        Abort
+    }
+
+    public interface ILink
+    {
+        bool IsEnabled { get; set; }
+        LinkResult Result { get; }
+        void RunLink();
+    }
+
+    public class Link : ILink
+    {
+        public bool IsEnabled
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public LinkResult Result
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public void RunLink()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
