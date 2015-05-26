0063453863aaeb92145a17256541e94247670ed3
Lucas Brendel <lucasbrendel@gmail.com>
2015-05-25 21:10:56 -0500

    adding unit test

---
+++ Chain.Test/ChainTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Chain;
+using NSubstitute;
+
+namespace Chain.Test
+{
+    [TestClass]
+    public class ChainTest
+    {
+        [TestMethod]
+        public void AddLinkTest()
+        {
+            ILink link = Substitute.For<ILink>();
+            Chain chain = new Chain();
+
+            Assert.AreEqual(0, chain.Links.Count);
+            chain.AddLink(link);
+
+            Assert.AreEqual(1, chain.Links.Count);
+        }
+    }
+}
