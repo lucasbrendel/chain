f689fbed1189010574d6eee2c756c5841e4fd599
Lucas Brendel <lucasbrendel@gmail.com>
2015-05-25 21:53:31 -0500

    Added more tests

--- Chain.Test/ChainTest.cs
+++ Chain.Test/ChainTest.cs
@@ -20,5 +20,52 @@ namespace Chain.Test
 
             Assert.AreEqual(1, chain.Links.Count);
         }
+
+        [TestMethod]
+        public void RunLinksWithEnabledLinkTest()
+        {
+            ILink link = Substitute.For<Link>();
+
+            Chain chain = new Chain();
+            chain.AddLink(link);
+
+            chain.RunLinks();
+
+            link.Received(1).HookBeforeLink();
+            link.Received(1).RunLink();
+            link.Received(1).HookAfterLink();
+        }
+
+        [TestMethod]
+        public void RunLinksWithDisabledLinkTest()
+        {
+            ILink link = Substitute.For<Link>();
+
+            link.IsEnabled = false;
+            Chain chain = new Chain();
+
+            chain.AddLink(link);
+
+            chain.RunLinks();
+
+            link.DidNotReceive().HookBeforeLink();
+            link.DidNotReceive().RunLink();
+            link.DidNotReceive().HookAfterLink();
+        }
+
+        [TestMethod]
+        public void LoopLinksTest()
+        {
+            ILink link = Substitute.For<Link>();
+            Chain chain = new Chain();
+
+            chain.AddLink(link);
+
+            chain.LoopLinks(3);
+
+            link.Received(3).HookBeforeLink();
+            link.Received(3).RunLink();
+            link.Received(3).RunLink();
+        }
     }
 }
